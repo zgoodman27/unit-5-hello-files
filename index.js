c43227b2d8fd563a06b54cd0240763ecdca8d4b9
zgoodman27 <zgoodman27@gmail.com>
2025-07-14 15:06:05 -0400

    finalized project

--- index.js
+++ index.js
@@ -3,6 +3,29 @@ const app = express();
 
 const PORT = 3000;
 
+app.use(express.static("public"))
+
+// Set up route to index
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname + "/public/index.html" )
+})
+
+// Set up route to herpetology
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname + "/public/herpetology.html" )
+})
+
+// Set up route to pyrology
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname + "/public/pyrology.html" )
+})
+
+app.get("/:key",(req,res)=> {
+    if (req.params = "amphibians") {
+        res.sendFile(__dirname + "/public/herpetology.html" )
+    }
+})
+
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`);
 });
